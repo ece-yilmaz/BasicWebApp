bf7f06c02514e3ddb7e74a8eef8f25cecad74078
Ece <ge47dim@mytum.de>
2022-07-15 15:38:39 +0200

    done

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -5,8 +5,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class QueryProcessor {
 
-    int highest;
-
     public String process(String query) {
 		query = query.toLowerCase();
         if (query.contains("shakespeare")) {
@@ -15,27 +13,32 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
             return "ecesapp";
-        } else if (query.contains("plus")) {
-            String [] strings = query.split(" ");
-            int result = Integer.parseInt(strings[strings.length - 1]) + Integer.parseInt(strings[strings.length - 3]);
-            return "" + result;
-        } else if (query.contains("multiplied")) {
-            String [] strings = query.split(" ");
-            int result = Integer.parseInt(strings[strings.length - 1]) * Integer.parseInt(strings[strings.length - 4]);
-            return "" + result;
-        } else if (query.contains("largest")) {
-            String [] subsection = query.split(": ");
-            String [] strings = subsection[1].split(", ");
-            Integer [] numbers = new Integer[strings.length];
-            for (int i = 0; i <= strings.length - 1; i++) {
-                numbers[i] = Integer.parseInt(strings[i]);
-            }
-            for (Integer integer : numbers) {
-                if(integer > highest) {
-                    highest = integer;
+        } else if (query.contains("which of the following numbers is the largest")) {
+        String numbers = query.substring("d956fe10: which of the following numbers is the largest: ".length());
+        String[] n = numbers.split(", ");
+        int largest = Integer.MIN_VALUE;
+        for (int i = 0; i < n.length; i++) {
+            try {
+                int num = Integer.parseInt(n[i]);
+                if(num > largest) {
+                    largest = num;
                 }
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
             }
-            return "" + highest;
+        }
+        return "" + largest;
+    } else if (query.contains("what is ")) {
+        String[] parts = query.split(" ");
+        int result = 0;
+        try {
+            int a = Integer.parseInt(parts[parts.length - 1]);
+            int b = Integer.parseInt(parts[parts.length - 3]);
+            result = a + b;
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+        return "" + result;
         } else { // TODO extend the programm here
             return "";
         }
