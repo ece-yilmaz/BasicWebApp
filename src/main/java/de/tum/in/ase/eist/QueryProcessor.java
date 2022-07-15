749a5a3db46b8f27adc61377a4fe26d2621cc2a0
Ece <ge47dim@mytum.de>
2022-07-15 15:28:52 +0200

    done

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Service;
 @Service
 public class QueryProcessor {
 
+    int highest;
+
     public String process(String query) {
 		query = query.toLowerCase();
         if (query.contains("shakespeare")) {
@@ -14,9 +16,28 @@ public class QueryProcessor {
         } else if (query.contains("name")) {
             return "ecesapp";
         } else if (query.contains("plus")) {
-            return "13";
+            String [] strings = query.split(" ");
+            int result = Integer.parseInt(strings[strings.length - 1]) + Integer.parseInt(strings[strings.length - 3]);
+            return "" + result;
+        } else if (query.contains("multiplied")) {
+            String [] strings = query.split(" ");
+            int result = Integer.parseInt(strings[strings.length - 1]) * Integer.parseInt(strings[strings.length - 4]);
+            return "" + result;
         } else if (query.contains("largest")) {
-            return "990";
+            String [] subsection = query.split(": ");
+            String [] strings = subsection[1].split(", ");
+            Integer [] numbers = new Integer[strings.length];
+            for (int i = 0; i <= strings.length - 1; i++) {
+                numbers[i] = Integer.parseInt(strings[i]);
+            }
+            for (Integer integer : numbers) {
+                if(integer > highest) {
+                    highest = integer;
+                }
+            }
+            return "" + highest;
+            // } else if (query.contains("name")) {
+            // return "Nico";
         } else { // TODO extend the programm here
             return "";
         }
